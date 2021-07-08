efbf305821c892047a8330671306ad40b97dc40f
Yasir Emin Çiftçi <yasireminciftci@gmail.com>
2021-07-08 16:07:52 +0300

    Trigger to hold the ball added, Simple Brick Prefab Added, Line renderer fixed.

--- Assets/Script/BallMovement.cs
+++ Assets/Script/BallMovement.cs
@@ -16,23 +16,35 @@ public class BallMovement : MonoBehaviour
     }
     void Update()
     {
-        if (rayCaster.isPress)
+        if (rayCaster.isPress && speed == 0)
         {
             direction = (rayCaster.hitPoint - transform.position).normalized;
+            line.enabled = true;
             line.SetPosition(0, transform.position);
             line.SetPosition(1, rayCaster.hitPoint);
         }
         transform.position += movementDirection * speed * Time.deltaTime;
 
     }
+
     private void OnMauseButtonUp()
     {
-        movementDirection = direction;
+        if (speed == 0)
+        {
+            movementDirection = direction;
+            speed = 10;
+            line.enabled = false;
+        }
+
     }
 
     private void OnCollisionEnter(Collision collision)
     {
         movementDirection = Vector3.Reflect(movementDirection, collision.GetContact(0).normal);
         Debug.Log(collision.transform.name);
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        speed = 0;
     }
 }
